3ce9696286f13a8008c4f4f419c28f9a8a88c428
Abhishek Nayek <abhishek.nayek@siemens.com>
2021-04-19 17:51:15 +0530

    Updates

---
+++ precache-manifest.d36dc5fa1a0085a09d456e3af49a2f8c.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d998f402cb1cfc9fa0fb9968860b0569",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3dac6a3ef122e9975b4a",
+    "url": "/static/css/main.956fad23.chunk.css"
+  },
+  {
+    "revision": "94833f0d9aaa3be6210f",
+    "url": "/static/js/2.19e99e5d.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/static/js/2.19e99e5d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3dac6a3ef122e9975b4a",
+    "url": "/static/js/main.b4ec4f90.chunk.js"
+  },
+  {
+    "revision": "f52ba179432c3be28857",
+    "url": "/static/js/runtime-main.554504a0.js"
+  },
+  {
+    "revision": "0ea05381ba89a67065b5aec1817cea64",
+    "url": "/static/media/abhisheknayek.0ea05381.jpg"
+  },
+  {
+    "revision": "5f25c85f567b6b2576104ede82f0c1e7",
+    "url": "/static/media/resume.5f25c85f.pdf"
+  },
+  {
+    "revision": "271b68811097c0beb0c7ec16ee8d24b6",
+    "url": "/static/media/siemens.271b6881.jpg"
+  }
+]);
